225f0526f617e43534d817bd637fd51fd7a7f124
Robbie Perry <robbieperry22@gmail.com>
2017-10-04 16:39:38 -0700

    Map

--- app/src/main/java/ca/bcit/ass1/newwesttraffictracker/MainActivity.java
+++ app/src/main/java/ca/bcit/ass1/newwesttraffictracker/MainActivity.java
@@ -1,6 +1,7 @@
 package ca.bcit.ass1.newwesttraffictracker;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
@@ -9,5 +10,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent i = new Intent(MainActivity.this, MapsActivity.class);
+        startActivity(i);
     }
 }
